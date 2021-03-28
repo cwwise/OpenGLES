28c2bccabf1fe86b0cbc296f9e0ed7fd1ea99611
chenwei08 <chenwei08@kuaishou.com>
2021-03-28 15:05:37 +0800

    update 绘制三角形

---
+++ OpenGLES02-着色器/OpenGLES02-着色器/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  OpenGLES02-着色器
+//
+//  Created by chenwei on 2021/3/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func loadView() {
+        self.view = OpenGLESView(frame: UIScreen.main.bounds)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
